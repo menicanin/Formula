98c6200730aa50ad46e38bb504af21292cfbc84a
Daniel Menićanin <101369794+menicanin@users.noreply.github.com>
2023-08-09 03:13:21 +0200

    Create ledFx.h

---
+++ ledFx.h
@@ -0,0 +1,25 @@
+#ifndef LEDFX_H
+#define LEDFX_H
+
+#include <Arduino.h>
+
+// Pin declarations
+extern const int dataPin;
+extern const int latchPin;
+extern const int clockPin;
+
+// Other variable declarations
+extern byte leds1;
+extern byte leds2;
+extern int t;
+extern int count;
+
+// Function declarations
+void updateShiftRegister(byte leds1, byte leds2);
+void ledFX(void (*fn)(), int count, void (*fn2)() = nullptr);
+void leftAnimation();
+void rightAnimation();
+void fillIn();
+void fillOut();
+
+#endif
